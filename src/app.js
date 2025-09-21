9ab12e9d3faa4e11131d6246f9b5549b105af5a5
Pedroporto87 <pedroporto_8@hotmail.com>
2025-09-21 18:45:00 -0300

    fix auth

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cookieParser from "cookie-parser";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from '../routes/auth.routes.js';
@@ -9,9 +10,11 @@ dotenv.config();
 
 const app = express();
 app.use(cors(({
-  origin: process.env.CORS_ORIGIN || "*", 
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+  credentials: true,
 })));
 app.use(express.json());
+app.use(cookieParser());
 //starter
 app.get('/starter', (_req, res) => {
     res.json({ status: "ok", message: 'Welcome to the Kaizen' });
